99f53ccb736b4c709547943c163158c4bc5c888e
wqr <wqrCoding@163.com>
2018-07-06 20:26:59 +0800

    Add when click,stay click mode

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@
 // 数组里是一个html里dom节点抽象的数据结构
 var keys = document.querySelectorAll('#calculator span');
 var screen = document.querySelector('.screen');
+var operator = document.querySelectorAll('.operator');
 //flag,如果flag==true，则代表之前的一个输入是运算符
 var flag = false;
 // 如果afterEval为真，表示刚进行过一次等号操作
@@ -12,7 +13,7 @@ var afterEval = false;
 
 for (var i = keys.length - 1; i >= 0; i--) {
 	// 拿到数组中的第i项
-	var element = keys[i];
+	var element = keys[i];	
 	// onclick指当按钮被点击时，会调用的函数
 	element.onclick = function (arg) {
 		// console.log(this.innerHTML);
@@ -44,7 +45,20 @@ for (var i = keys.length - 1; i >= 0; i--) {
 				flag = true;
 				//将afterEval还原成false
 				afterEval = false;
+				//按下的时候，运算符保持按下状态
+				this.style.backgroundColor = "#9c89f6";
+				this.style.boxShadow="none";
+				this.style.color="white";
 			}else {
+				// 如果数字的上一次是符号
+				if(flag){
+					// 把所有的符号都变回原来的模式
+					for (var i = operator.length - 1; i >= 0; i--) {
+						operator[i].style.backgroundColor = "#FFF0F5";
+						operator[i].style.boxShadow="0px 4px rgba(0,0,0,2)";
+						operator[i].style.color="#888";	
+					}
+				}
 				// 这次不是运算符
 				flag = false;
 				//刚进行一次等号操作，则再输入数字的话屏幕清空
@@ -58,3 +72,4 @@ for (var i = keys.length - 1; i >= 0; i--) {
 		}
 	}
 }
+
